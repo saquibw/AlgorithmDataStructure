c63f4fb71d5045cf025e987d5d19ba8c20f93a1b
saquib-aug <saquib@augmedix.com>
2019-05-29 16:15:47 +0600

    Binary search done again

--- src/com/learning/algorithm/BinarySearch.java
+++ src/com/learning/algorithm/BinarySearch.java
@@ -8,42 +8,33 @@ public class BinarySearch {
 		
 		Integer[] array = {1,3,5,6,8,9,11,12,13,15,18,19,22,25,26,29,32,35,39,42,44,45,48,49,50,52,56,59,60,72,75,76,78,79,80,82,83};
 		
-		int startIndex = 0;
-		int endIndex = array.length-1;
-		
-		int iteration = 1;
-		
-		System.out.println("Please enter the number to find: ");
 		Scanner sc = new Scanner(System.in);
-		int input = sc.nextInt();
-		System.out.println("You entered: " + input);
+		System.out.println("Please input a number: ");
+		int num = sc.nextInt();
+		System.out.println("Input number: " + num);
 		
-		while (startIndex <= endIndex) {
-			int midIndex = (endIndex + startIndex) / 2;
-			/*if(iteration == 10) {
-				return;
-			}*/
-			System.out.println("Interation: " + iteration);
-			System.out.println("Start index: " + array[startIndex]);
-			System.out.println("Mid index: " + array[midIndex]);
-			System.out.println("End index: " + array[endIndex]);
-			System.out.println("===============================");
-			
-			if(input == array[midIndex]) {
-				System.out.println("Number found at index: " + midIndex);
+		int left = 0;
+		int right = array.length-1;
+		
+		
+		//System.out.println("Initial mid: " + mid);
+		
+		while(left <= right) {
+			int mid = (left + right)/2;
+			System.out.println("Middle number: " + array[mid]);
+			if(array[mid] == num) {
+				System.out.println("The number found at index " + mid);
 				return;
 			}
 			
-			if(array[midIndex] < input) {
-				startIndex = midIndex+1;
+			if(num > array[mid]) {
+				left = mid+1;
 			} else {
-				endIndex = midIndex-1;
-			}
-			iteration++;
+				right = mid-1;
+			}			
 		}
-		System.out.println("Number not found");
+	System.out.println("Number not found!");
+	return;
+	
 	}
-	
-	
-
 }
